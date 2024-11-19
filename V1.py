311d1e63f8d954350b4d5be41aa96a4166330043
AlexandreGastinel <gastinel.alex@gmail.com>
2024-11-19 16:05:33 +0100

    Renvoie liste des signaux decoupees en fenetres

--- V1.py
+++ V1.py
@@ -1,29 +1,45 @@
 import librosa
 import os
+import numpy as np
+
+
+
+    ### EXTRACTION DES SIGNAUX ###
 
 chemin_dossier = r"C:\Users\Alex\Desktop\CS\2A\Projet IA\recognaition\DATASET\wav"
-enregistrements = os.listdir(chemin_dossier)
+fichiers_audios = os.listdir(chemin_dossier)
 
+enregistrements = []
 
-
-for e in enregistrements:
-    e_path = os.path.join(chemin_dossier,e)
+for f in fichiers_audios:
+    f_path = os.path.join(chemin_dossier,f)
+    data, sr = librosa.load(f_path)
+    enregistrements.append(data)
     """
-    print(f"traitement du fichier : {e}")
+    print(f"traitement du fichier : {f}")
 
     try :
-        data, sr = librosa.load(e_path, sr = None)
+        data, sr = librosa.load(f_path, sr = None)
         print(sr)
         print(f"La durer de l'enregistrement est de {len(data) /sr} secondes") 
     except Exception as exception:
-        print(f"Erreur lors du traitement de {e} : {exception}")
+        print(f"Erreur lors du traitement de {f} : {exception}")
    """
-    
 
 
-### Nous allons découper chaque signal en fenêtre de durer T"
+    ### DIVISION DES SIGNAUX EN FENETRES ###
 
-T = 0.02   # En secondes 
-_, sr = librosa.load(os.path.join(chemin_dossier,enregistrements[0]), sr = None)
-nb_donnees = T * sr
-print(T, sr, nb_donnees)
+# fenetre de T secondes 
+T = 0.02
+_, sr = librosa.load(os.path.join(chemin_dossier,fichiers_audios[0]), sr = None)
+nb_donnees_fenetre = int(T * sr)
+nb_chevauchement_fenetre = nb_donnees_fenetre   # gère le chevauchement entre les fenetres
+
+enregistrements_decoupes = []
+for e in enregistrements:
+    fenetres = librosa.util.frame(data, frame_length=nb_donnees_fenetre, hop_length=nb_chevauchement_fenetre)
+    fenetres = np.transpose(fenetres)
+    enregistrements_decoupes.append(fenetres)
+    # fenetre est un array dont chaque ligne est une fenetre 
+
+print(len(enregistrements) == len(enregistrements_decoupes))
